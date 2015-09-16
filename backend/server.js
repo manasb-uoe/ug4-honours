3170a2a120d89ed08f3c637c1fbd54369957d967
Manas Bajaj <manas.bajaj94@gmail.com>
2015-09-16 21:29:53 +0530

    [backend] Added new middleware: 404_error_handler, which sends custom API response to client when no route matches the client's request.

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ var config = require("./config");
 var responsesMiddleware = require("./middleware/responses");
 var userRoutes = require("./controllers/user_controller");
 var authRoutes = require("./controllers/auth_controller");
+var errorHandlerMiddleware = require("./middleware/404_error_handler");
 
 /**
  * Configuration
@@ -31,16 +32,15 @@ mongoose.connect(config.database);
 // define port for the HTTP server (give priority to PORT environment variable)
 var port = process.env.PORT || 4000;
 
-// use custom middleware
+// add custom methods for sending API responses to res object
 app.use(responsesMiddleware);
 
-
-/**
- * Register API routes. All routes will be prefixed with /api.
- */
-
+// Register API routes. All routes will be prefixed with /api.
 app.use("/api", userRoutes);
 app.use("/api", authRoutes);
+
+// add 404 error handling middleware in order to send custom message when no route matches client's request
+app.use(errorHandlerMiddleware);
 
 
 /**
