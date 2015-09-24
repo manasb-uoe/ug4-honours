91a555df0f86f0bfebfd167424cdeb380ad046ac
Manas Bajaj <manas.bajaj94@gmail.com>
2015-09-24 11:03:29 +0100

    [backend] Added tests for testing GET request to api/users/user_id.

--- backend/tests/test_suite.js
+++ backend/tests/test_suite.js
@@ -8,6 +8,7 @@ var assert = require("chai").assert;
 var util = require("./util");
 var server = require("../server");
 var supertest = require("supertest");
+var authTokenService = require("../services/auth_token");
 
 server.start(server.ApiExecutionModeEnum.TEST); // start server in test execution mode
 var api = supertest(server.app);
@@ -301,6 +302,8 @@ describe("Test suite", function () {
                             .end(function (err, res) {
                                 if (err) return done(err);
 
+                                assert.equal(res.body.success, true);
+
                                 // make sure that the returned details match, and only required fields are returned
                                 var users = res.body.data;
                                 assert.equal(users.length, 1);
@@ -309,6 +312,69 @@ describe("Test suite", function () {
                                 assert.isDefined(users[0].createdAt);
                                 assert.isUndefined(users[0].password);
                                 assert.isDefined(users[0].id);
+
+                                return done();
+                            });
+                    });
+                });
+            });
+        });
+
+        describe("GET api/users/user_id", function () {
+
+            it("should return unauthorized error when user is not authenticated", function (done) {
+                api.get("/api/users/bla")
+                    .expect("Content-Type", /json/)
+                    .expect(401)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+
+                        assert.equal(res.body.success, false);
+                        assert.equal(res.body.error.code, 401);
+
+                        return done();
+                    });
+            });
+
+            it("should return not found error when user is authenticated but user_id param is incorrect",
+                function (done) {
+                    util.createUser(api, util.user1_sample_data, function (err, res) {
+                        if (err) return done(err);
+
+                        api.get("/api/users/bla")
+                            .set({"Authorization": "Bearer " + res.body.data.token})
+                            .expect(404)
+                            .end(function (err, res) {
+                                if (err) return done(err);
+                                assert.equal(res.body.success, false);
+                                return done();
+                            });
+                    });
+                });
+
+            it("should return user when user is authenticated and user_id param is correct", function (done) {
+                util.createUser(api, util.user1_sample_data, function (err, res) {
+                    if (err) return done(err);
+
+                    // retrieve user id from token
+                    authTokenService.verifyToken(res.body.data.token, function (err, decoded) {
+                       if (err) return done(err);
+
+                        api.get("/api/users/" + decoded.id)
+                            .set({"Authorization": "Bearer " + res.body.data.token})
+                            .expect(200)
+                            .end(function (err, res) {
+                                if (err) return done(err);
+
+                                assert.equal(res.body.success, true);
+
+                                // make sure that the returned details match, and only required fields are returned
+                                var user = res.body.data;
+                                assert.equal(user.name, util.user1_sample_data.name);
+                                assert.equal(user.email, util.user1_sample_data.email);
+                                assert.isDefined(user.createdAt);
+                                assert.isUndefined(user.password);
+                                assert.isDefined(user.id);
 
                                 return done();
                             });
