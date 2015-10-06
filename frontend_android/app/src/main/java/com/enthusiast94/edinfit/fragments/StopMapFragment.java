d9371cc460ace318f879f3a5a8f11e93cf4296a2
Manas Bajaj <manas.bajaj94@gmail.com>
2015-10-06 19:52:11 +0100

    [frontend_android] Added ability to display map view in StopMapFragment along with user's current location.

--- frontend_android/app/src/main/java/com/enthusiast94/edinfit/fragments/StopMapFragment.java
+++ frontend_android/app/src/main/java/com/enthusiast94/edinfit/fragments/StopMapFragment.java
@@ -8,16 +8,67 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.enthusiast94.edinfit.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 
 /**
  * Created by manas on 04-10-2015.
  */
 public class StopMapFragment extends Fragment {
 
+    private MapView mapView;
+    private GoogleMap map;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stop_map, container, false);
+
+        /**
+         * Find and create map view
+         */
+
+        mapView = (MapView) view.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+
+        /**
+         * Get GoogleMap from MapView and initialize it
+         */
+
+        map = mapView.getMap();
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.setMyLocationEnabled(true);
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
