aa6658dbe23dd514465ad3f516f6373c80726374
Manas Bajaj <manas.bajaj94@gmail.com>
2015-10-19 10:04:27 +0100

    [frontend_android] Updated GoFragment to add ability to navigate to Wait or Walk activity.

--- frontend_android/app/src/main/java/com/enthusiast94/edinfit/fragments/GoFragment.java
+++ frontend_android/app/src/main/java/com/enthusiast94/edinfit/fragments/GoFragment.java
@@ -1,5 +1,6 @@
 package com.enthusiast94.edinfit.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,16 +9,48 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.enthusiast94.edinfit.R;
+import com.enthusiast94.edinfit.activities.WaitOrWalkActivity;
+import com.google.android.gms.games.multiplayer.realtime.WaitingRoomListener;
 
 /**
  * Created by manas on 01-10-2015.
  */
-public class GoFragment extends Fragment {
+public class GoFragment extends Fragment implements View.OnClickListener {
+
+    private View waitOrWalkView;
+    private View getToSomewhereView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_go, container, false);
+
+        /**
+         * Find views
+         */
+
+        waitOrWalkView = view.findViewById(R.id.wait_or_walk_view);
+        getToSomewhereView = view.findViewById(R.id.get_to_somewhere_view);
+
+        /**
+         * Bind event listeners
+         */
+
+        waitOrWalkView.setOnClickListener(this);
+        getToSomewhereView.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.wait_or_walk_view:
+                Intent goToActivityIntent = new Intent(getActivity(), WaitOrWalkActivity.class);
+                startActivity(goToActivityIntent);
+                break;
+            case R.id.get_to_somewhere_view:
+                break;
+        }
     }
 }
