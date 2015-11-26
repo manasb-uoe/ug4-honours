655b8c1f0124bfb8e31e30b0af7853ce59aff1de
Manas Bajaj <manas.bajaj94@gmail.com>
2015-11-26 22:40:12 +0000

    [backend] Added new route GET /activities to fetch all of currently authenticated user's activities.

--- backend/controllers/activity_controller.js
+++ backend/controllers/activity_controller.js
@@ -9,38 +9,47 @@ var authenticationMiddleware = require("../middleware/authentication");
 
 var router = express.Router();
 
-router.post("/activities", authenticationMiddleware, function (req, res) {
-    var activityJson = req.body.activity;
+router.route("/activities")
 
-    if (!activityJson) {
-        return res.sendError(400, "activity is a required field");
-    }
+    .post(authenticationMiddleware, function (req, res) {
+        var activityJson = req.body.activity;
 
-    User.findById(req.decodedPayload.id, function (err, user) {
-        if (err) return res.sendError(500, err.message);
-
-        try {
-            var activity = JSON.parse(activityJson);
-        } catch (e) {
-            return res.sendError(400, "Failed to parse activity");
+        if (!activityJson) {
+            return res.sendError(400, "activity is a required field");
         }
 
-        var activityObj = new Activity(activity);
-        var validationErr = activityObj.validateInfo();
-
-        if (validationErr) {
-            return res.sendError(400, validationErr.message);
-        }
-
-        user.activities.push(activityObj);
-
-        user.save(function (err) {
+        User.findById(req.decodedPayload.id, function (err, user) {
             if (err) return res.sendError(500, err.message);
 
-            return res.sendOk();
+            try {
+                var activity = JSON.parse(activityJson);
+            } catch (e) {
+                return res.sendError(400, "Failed to parse activity");
+            }
+
+            var activityObj = new Activity(activity);
+            var validationErr = activityObj.validateInfo();
+
+            if (validationErr) {
+                return res.sendError(400, validationErr.message);
+            }
+
+            user.activities.push(activityObj);
+
+            user.save(function (err) {
+                if (err) return res.sendError(500, err.message);
+
+                return res.sendOk();
+            });
+        });
+    })
+
+    .get(authenticationMiddleware, function (req, res) {
+        User.findById(req.decodedPayload.id, function (err, user) {
+            if (err) return res.sendError(500, err.message);
+
+            return res.sendOk(user.activities);
         });
     });
-});
-
 
 module.exports = router;
