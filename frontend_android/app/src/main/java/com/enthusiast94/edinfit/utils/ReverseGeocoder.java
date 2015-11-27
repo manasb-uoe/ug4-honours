6ed4445b005fe272f48368045d5a71f977d3b334
Manas Bajaj <manas.bajaj94@gmail.com>
2015-11-27 00:10:45 +0000

    [frontend_android] Added utility class for reverse geocoding coordinates.

---
+++ frontend_android/app/src/main/java/com/enthusiast94/edinfit/utils/ReverseGeocoder.java
@@ -0,0 +1,73 @@
+package com.enthusiast94.edinfit.utils;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Handler;
+import android.util.Log;
+
+import com.enthusiast94.edinfit.R;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by manas on 26-11-2015.
+ */
+public class ReverseGeocoder  {
+
+    private static final String TAG = ReverseGeocoder.class.getSimpleName();
+
+    private Geocoder geocoder;
+    private Handler handler;
+    private String errorMessage;
+
+    public ReverseGeocoder(Context context) {
+        geocoder = new Geocoder(context);
+        handler = new Handler();
+        errorMessage = context.getString(R.string.error_could_not_geocode_location);
+    }
+
+    public interface ReverseGeocodeCallback {
+        void onSuccess(String placeName);
+        void onFailure(String error);
+    }
+
+    public void getPlaceName(final double lat, final double lng,
+                             final ReverseGeocodeCallback callback) {
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                List<Address> list = null;
+
+                try {
+                    list = geocoder.getFromLocation(lat, lng, 1);
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+
+                if (list != null && list.size() > 0) {
+                    final List<Address> finalList = list;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            callback.onSuccess(finalList.get(0).getThoroughfare());
+                        }
+                    });
+
+                    return;
+                }
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onFailure(errorMessage);
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+}
